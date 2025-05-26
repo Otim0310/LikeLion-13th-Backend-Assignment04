ff2cb69f4f18f8f583a15921eee40be87c049c51
Otim0310 <rkdmsdlaa123@naver.com>
2025-05-26 19:43:01 +0900

    Update PatientListResponseDto.java

--- geaun/LikeLion13thAssignment03/src/main/java/com/likelion/likelionassignment03/patient/api/dto/response/PatientListResponseDto.java
+++ geaun/LikeLion13thAssignment03/src/main/java/com/likelion/likelionassignment03/patient/api/dto/response/PatientListResponseDto.java
@@ -1,19 +1,17 @@
 package com.likelion.likelionassignment03.patient.api.dto.response;
 
+import com.likelion.likelionassignment03.patient.api.dto.response.PatientInfoResponseDto;
 import lombok.Builder;
 
-import java.util.Collections;
 import java.util.List;
-import java.util.Objects;
 
 @Builder
 public record PatientListResponseDto(
         List<PatientInfoResponseDto> patients
 ) {
-    public static PatientListResponseDto fromPatientDtoList(List<PatientInfoResponseDto> patients) {
-        List<PatientInfoResponseDto> safeList = Objects.requireNonNullElse(patients, Collections.emptyList());
+    public static PatientListResponseDto from(List<PatientInfoResponseDto> patients) {
         return PatientListResponseDto.builder()
-                .patients(safeList)
+                .patients(patients)
                 .build();
     }
 }
